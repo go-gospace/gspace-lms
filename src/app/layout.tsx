849c32c2ab2d9824fd0e49b067674949786a2163
thehsadowte <theshadowte@gmail.com>
2024-08-23 00:57:43 +0300

    added logo, starter routes and few components

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,8 +5,8 @@ import "./globals.css";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Lama Dev School Management Dashboard",
-  description: "Next.js School Management System",
+  title: "GoGoSpace LMS",
+  description: "GSpace Learning Management System",
 };
 
 export default function RootLayout({
