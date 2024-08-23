829764c3703d90a561f4cc1ccad5dc02be4d4b69
thehsadowte <theshadowte@gmail.com>
2024-08-24 01:08:47 +0300

    added calendars, charts, menu

---
+++ src/components/BigCalendar.tsx
@@ -0,0 +1,42 @@
+"use client";
+import dynamic from "next/dynamic";
+
+import { Calendar, momentLocalizer, View, Views } from "react-big-calendar";
+import moment from "moment";
+import { calendarEvents } from "@/lib/data";
+import "react-big-calendar/lib/css/react-big-calendar.css";
+// import "moment/locale/uk";
+import { useEffect, useState } from "react";
+
+const localizer = momentLocalizer(moment);
+
+const BigCalendar = () => {
+  const [view, setView] = useState<View>(Views.WORK_WEEK);
+
+  const handleOnChangeView = (selectedView: View) => {
+    setView(selectedView);
+  };
+
+  //   useEffect(() => {
+  //     moment.locale("uk");
+  //   }, []);
+
+  return (
+    <>
+      <Calendar
+        localizer={localizer}
+        events={calendarEvents}
+        startAccessor="start"
+        endAccessor="end"
+        views={["work_week", "day"]}
+        view={view}
+        style={{ height: "98%" }}
+        onView={handleOnChangeView}
+        min={new Date(2025, 1, 0, 8, 0, 0)}
+        max={new Date(2025, 1, 0, 17, 0, 0)}
+      />
+    </>
+  );
+};
+
+export default BigCalendar;
